67b4a2f8c7c3a44144bf0b35e3af790fe8b05ef5
James Sutton <1892175+zeryx@users.noreply.github.com>
2021-12-21 13:19:43 -0400

    initial commit, algorithm created and training pipeline functional

--- src/pytorch_training_dummy.py
+++ src/pytorch_training_dummy.py
@@ -1,56 +1,41 @@
 import Algorithmia
 import torch as th
-
-"""
-Example Input:
-{
-    "matrix_a": [[0, 1], [1, 0]],
-    "matrix_b": [[25, 25], [11, 11]]
-}
-
-Expected Output:
-{
-    "product": [[11, 11], [25, 25]]
-}
-"""
-
-class InputObject:
-    def __init__(self, input_dict):
-        """
-        Creates an instance of the InputObject, which checks the format of data and throws exceptions if anything is
-        missing.
-        "matrix_a" and "matrix_b" must be the same shape.
-        :param A - Matrix A, converted from a json list into a torch cuda Tensor.
-        :param B - Matrix B, converted from a json list into a torch cuda Tensor.
-        """
-        if isinstance(input_dict, dict):
-            if {'matrix_a', 'matrix_b'} <= input_dict.keys():
-                self.A = convert(input_dict['matrix_a'])
-                self.B = convert(input_dict['matrix_b'])
-            else:
-                raise Exception("'matrix_a' and 'matrix_b' must be defined.")
-        else:
-            raise Exception('input must be a json object.')
-        if self.A.shape[-1] != self.B.shape[0]:
-            raise Exception('inner dimensions between A and B must be the same.\n A: {} B: {}'
-                .format(self.A.shape[-1], self.B.shape[0]))
+from PIL import Image
+from torchvision import transforms
+import numpy as np
 
 
-def convert(list_array):
-    """
-    Converts a json list into a torch Tensor object.
-    """
-    th_tensor = th.tensor(list_array).float()
-    gpu_tensor = th_tensor.cuda()
-    return gpu_tensor
+def process_image(image_url, client):
+    local_image = client.file(image_url).getFile().name
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    img = Image.open(local_image)
+    img.load()
+    # If the image isn't a square, make it a square
+    if img.size[0] != img.size[1]:
+        sqrWidth = np.ceil(np.sqrt(img.size[0] * img.size[1])).astype(int)
+        image_data = transform(img.resize((sqrWidth, sqrWidth)))
+    else:
+        image_data = transform(img)
+    return image_data
 
-def apply(input):
+
+def load(state):
+    state['classes'], state['model'] = th.load(state.get_model('cifar10'))
+    return state
+
+
+def apply(input, state):
     """
     Calculates the dot product of two matricies using pytorch, with a cudnn backend.
     Returns the product as the output.
     """
-    input = InputObject(input)
-    C = th.mm(input.A, input.B)
-    z = C.cpu().numpy().tolist()
-    output = {'product': z}
+    image_data = process_image(input, state.client)
+    preds = state['model'](image_data)
+    _, predicted = th.max(preds.data, 1)
+    output = []
+    for j in range(len(state['classes'])):
+        prediction = {"class": state['classes'][preds[j]]}
+        output.append(prediction)
     return output
